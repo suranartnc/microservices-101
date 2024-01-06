ca25935e4f9d9294f8fa51fadaf5e120588f2d7d
Suranart Niamcome <suranart@tencent.co.th>
2024-01-06 15:57:16 +0700

    Add a spring tutorial

---
+++ spring/quickstart/src/main/java/com/suranartn/quickstart/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.suranartn.quickstart;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping(path = "/hello")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
