7f69cdfc328d428490c1b8f8f43d2f70be3cfa1b
Suranart Niamcome <suranart@tencent.co.th>
2024-01-07 16:41:33 +0700

    Load data from config instead

--- spring/quickstart/src/main/java/com/suranartn/pizza/PizzaApplication.java
+++ spring/quickstart/src/main/java/com/suranartn/pizza/PizzaApplication.java
@@ -10,18 +10,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @Log
 public class PizzaApplication implements CommandLineRunner {
+    final PizzaConfig pizzaConfig;
+
+    public PizzaApplication(PizzaConfig pizzaConfig) {
+        this.pizzaConfig = pizzaConfig;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(PizzaApplication.class, args);
     }
 
     @Override
     public void run(final String... args) {
-        final PizzaConfig pizzaConfig = new PizzaConfig(
-                "tomato",
-                "mozzarella",
-                "thin"
-        );
-
         log.info(
                 String.format("I want a %s crust pizza, with %s and %s sauce",
                 pizzaConfig.getCrust(),
--- spring/quickstart/src/main/java/com/suranartn/pizza/config/PizzaConfig.java
+++ spring/quickstart/src/main/java/com/suranartn/pizza/config/PizzaConfig.java
@@ -3,7 +3,11 @@ package com.suranartn.pizza.config;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ConfigurationProperties(prefix = "pizza")
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
