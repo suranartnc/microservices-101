a7d9a5566f0f46984b6899d71c7195f26578cc40
Suranart Niamcome <suranart.n@scbx.com>
2024-01-09 22:06:12 +0700

    Add get author by id controller

--- spring/database/src/main/java/com/suranartn/database/services/AuthorService.java
+++ spring/database/src/main/java/com/suranartn/database/services/AuthorService.java
@@ -3,9 +3,12 @@ package com.suranartn.database.services;
 import com.suranartn.database.domain.entities.AuthorEntity;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AuthorService {
     AuthorEntity createAuthor(AuthorEntity authorEntity);
 
     List<AuthorEntity> findAll();
+
+    Optional<AuthorEntity> findOne(Long id);
 }
